6818541d2b85a8938ed04ed9901c85eaec8cef1a
indexGuilherme <guidacruz2004@gmail.com>
2022-10-10 21:44:25 -0300

    criando o schema do usuário

--- src/users/schemas/users.schema.ts
+++ src/users/schemas/users.schema.ts
@@ -1,1 +1,27 @@
-export const UserSchema
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+export const UsersSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
+    password: {
+        type: String,
+    }
+});
+
+UsersSchema.pre('save', async function(next: mongoose.CallbackWithoutResultAndOptionalError){
+    try {
+        if(!this.isModified('password')) {
+            return next();
+        }
+
+        this['password'] = await bcrypt.hash(this['password'], 10);
+    }
+    catch(error) {
+        return next(error);
+    }
+});
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Mongoose } from 'mongoose';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersSchema } from './schemas/users.schema';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{
+      name: 'User',
+      schema: UsersSchema,
+    }]),
+    AuthModule,
+  ],
   controllers: [UsersController],
   providers: [UsersService]
 })
