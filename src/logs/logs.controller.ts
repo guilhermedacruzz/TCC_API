2a1753b92f8cf6b50ab8d78a028887471db9826c
indexGuilherme <guidacruz2004@gmail.com>
2022-11-23 07:29:43 -0300

    pequenas mudanças

--- src/logs/logs.controller.ts
+++ src/logs/logs.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { CreateDto } from './dto/create.dto';
+import { FindByIotIdDto } from './dto/findByIotId.dto';
 import { LogsService } from './logs.service';
+import { Log } from './models/logs.model';
 
 @Controller('logs')
 export class LogsController {
@@ -11,5 +13,11 @@ export class LogsController {
     @HttpCode(HttpStatus.CREATED)
     public async create(@Body() createDto: CreateDto) {
         return this.logsService.create(createDto);
+    }
+
+    @Get('byIotId')
+    @HttpCode(HttpStatus.OK)
+    public async findById(@Body() findByIotIdDto: FindByIotIdDto): Promise<Log[]> {
+        return this.logsService.findByIotId(findByIotIdDto);
     }
 }
--- src/logs/logs.service.ts
+++ src/logs/logs.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateDto } from './dto/create.dto';
+import { FindByIotIdDto } from './dto/findByIotId.dto';
 import { Log } from './models/logs.model';
 import { ResLogs } from './models/resLogs.model';
 
@@ -17,5 +18,20 @@ export class LogsService {
         log.save();
 
         return { _id: log.id, minutes: log.minutes, status: log.status, date: log.date };
+    }
+
+    private async findAll(): Promise<Log[]> {
+        return this.logsModel.find();
+    } 
+
+    public async findByIotId(findByIotId: FindByIotIdDto) : Promise<Log[]> {
+        const allLogs = await this.findAll();
+        var logsById = [];
+        for(var i = 0; i < allLogs.length; i++) {
+            if(allLogs[i].iot == findByIotId.id) {
+                logsById.push(allLogs[i]);
+            }
+        }
+        return logsById;
     }
 }
